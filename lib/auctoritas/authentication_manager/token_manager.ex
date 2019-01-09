072f5c34db87b2a88ab37d2e427ff81b5b0f9802
nkyian <nkyian@fastmail.com>
2019-01-10 01:56:34 +0200

    Default token_manager implementation

--- lib/auctoritas/authentication_manager/token_manager.ex
+++ lib/auctoritas/authentication_manager/token_manager.ex
@@ -1,11 +1,73 @@
 defmodule Auctoritas.AuthenticationManager.TokenManager do
   alias Auctoritas.TokenGenerator
+  alias Auctoritas.AuthenticationManager.DataStorage
 
   @type token() :: String.t()
 
   @callback generate_token(any()) :: {atom(), token()}
   @spec generate_token(any()) :: {atom(), token()}
-  def generate_token(data) do
+  def generate_token(_data) do
     {:ok, TokenGenerator.generate_token()}
+  end
+
+  @callback authentification_data_check(any()) :: {atom(), any()}
+  @spec authentification_data_check(any()) :: {atom(), any()}
+  def authentification_data_check(data) do
+    case data do
+      nil -> {:error, "Data is nil"}
+      data -> {:ok, data}
+    end
+  end
+
+  @callback data_check(any()) :: {atom(), any()}
+  @spec data_check(any()) :: {atom(), any()}
+  def data_check(data) do
+    case data do
+      nil -> {:error, "Data is nil"}
+      data -> {:ok, data}
+    end
+  end
+
+  @callback authenticate(any(), any()) :: {atom(), token(), any()}
+  @spec authenticate(any(), any()) :: {atom(), token(), any()}
+  def authenticate(authentification_data, saved_data) do
+    with {:ok, authentification_data} <- authentification_data_check(authentification_data),
+         {:ok, data} <- data_check(saved_data) do
+      {:ok, token} = generate_token(data)
+      DataStorage.insert_data(token, data)
+      {:ok, token, data}
+    else
+      {:error, error} -> {:error, error}
+    end
+  end
+
+  @spec login(any(), any()) :: {atom(), token(), any()}
+  def login(authentification_data, saved_data) do
+    authenticate(authentification_data, saved_data)
+  end
+
+  @callback deauthenticate(token()) :: {atom(), any()}
+  @spec deauthenticate(token()) :: {atom(), any()}
+  def deauthenticate(token) when is_bitstring(token) do
+    case DataStorage.delete_token(token) do
+      {:ok, data} -> {:ok, data}
+      {:error, error} -> {:error, error}
+    end
+  end
+
+  @spec logout(token()) :: {atom(), any()}
+  def logout(token) when is_bitstring(token) do
+    deauthenticate(token)
+  end
+
+  @callback get_token_data(token()) :: {atom(), any(), number()}
+  @spec get_token_data(token()) :: {atom(), any()}
+  def get_token_data(token) when is_bitstring(token) do
+    with {:ok, token_data} <- DataStorage.get_token_data(token),
+         {:ok, expiration} <- DataStorage.token_expires?(token) do
+      {:ok, token_data, expiration}
+    else
+      {:error, error} -> {:error, error}
+    end
   end
 end
