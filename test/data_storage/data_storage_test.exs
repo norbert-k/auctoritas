48209b3b24d8dff0717fadafa8a76874a1abf944
nkyian <nkyian@fastmail.com>
2019-01-10 11:34:18 +0200

    Tests fixed (from insert_data to insert_token)

--- test/data_storage/data_storage_test.exs
+++ test/data_storage/data_storage_test.exs
@@ -9,7 +9,7 @@ defmodule AuctoritasTest.DataStorageTest do
   @default_supervisor AuthenticationSupervisor.start_link(@config)
 
   test "insert data into data_storage" do
-    assert DataStorage.insert_data("sample_token", "sample_data") == {:ok, true}
+    assert DataStorage.insert_token("sample_token", "sample_data") == {:ok, true}
     assert DataStorage.get_token_data("sample_token") == {:ok, "sample_data"}
     assert DataStorage.get_tokens(0, 1) == {:ok, ["sample_token"]}
   end
