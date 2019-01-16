afc8d9ee9a6baa75df6124ddc45c271bd4fd8f14
nkyian <nkyian@fastmail.com>
2019-01-16 22:49:48 +0200

    Version bump (to 0.9.0)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Auctoritas.MixProject do
   def project do
     [
       app: :auctoritas,
-      version: "0.4.0",
+      version: "0.9.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
